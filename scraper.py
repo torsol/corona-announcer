d98cf368e5842737165a95ba9ba94e437f9ca697
Torstein Solberg <torstsol@stud.ntnu.no>
2020-03-13 22:16:43 +0100

    added original scraper for rPi, pings VG for live corona-data and plays a flushing sound if the number has increased

---
+++ scraper.py
@@ -0,0 +1,26 @@
+import requests
+import json
+from datetime import date
+import time
+import pygame
+
+pygame.mixer.init()
+pygame.mixer.music.load("toilet.mp3")
+pygame.mixer.music.play()
+while pygame.mixer.music.get_busy() == True:
+    continue
+
+
+number = json.loads(requests.get('https://www.vg.no/spesial/2020/corona-viruset/data/norway/').text)[
+    "timeseries"]["total"]["confirmed"][str(date.today())]
+while(True):
+    time.sleep(5)
+    new_number = json.loads(requests.get('https://www.vg.no/spesial/2020/corona-viruset/data/norway/').text)["timeseries"]["total"]["confirmed"][str(date.today())]
+    if(number < new_number):
+        number = new_number
+        print("updated: " + str(number))
+        pygame.mixer.music.play()
+        while pygame.mixer.music.get_busy() == True:
+            continue
+    else:
+        print("not updated: " + str(number))

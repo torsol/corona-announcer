341b9493fabc224d3ed07fcbe02f9e09b6c7f84f
Torstein Solberg <torstsol@stud.ntnu.no>
2020-03-13 22:50:36 +0100

    more reformatting

--- scraper.py
+++ scraper.py
@@ -4,22 +4,26 @@ from datetime import date
 import time
 import pygame
 
+def play_sound():
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy():
+        continue
+
+
+def fetch_data(): return json.loads(requests.get('https://www.vg.no/spesial/2020/corona-viruset/data/norway/').text)[
+        "timeseries"]["total"]["confirmed"][str(date.today())]
+
 pygame.mixer.init()
 pygame.mixer.music.load("toilet.mp3")
-pygame.mixer.music.play()
-while pygame.mixer.music.get_busy():
-    continue
+play_sound()
 
-number = json.loads(requests.get('https://www.vg.no/spesial/2020/corona-viruset/data/norway/').text)[
-    "timeseries"]["total"]["confirmed"][str(date.today())]
+number = fetch_data()
 while(True):
     time.sleep(5)
-    new_number = json.loads(requests.get('https://www.vg.no/spesial/2020/corona-viruset/data/norway/').text)["timeseries"]["total"]["confirmed"][str(date.today())]
+    new_number = fetch_data()
     if(number < new_number):
         number = new_number
         print("updated: " + str(number))
-        pygame.mixer.music.play()
-        while pygame.mixer.music.get_busy():
-            continue
+        play_sound()
     else:
         print("not updated: " + str(number))

0f0a465be2e3bab4a17a5571a2a78dc036b1f026
Torstein Solberg <torstsol@stud.ntnu.no>
2020-03-19 15:02:26 +0100

    added the different stats in a dictionary for cleaner code

--- scraper.py
+++ scraper.py
@@ -19,7 +19,6 @@ def play_sound(soundFile):
     while pygame.mixer.music.get_busy():
         continue
 
-
 # API for hospital-info
 def fetch_hospital_data(): return json.loads(requests.get(
     'https://redutv-api.vg.no/corona/v1/hospitalized').text)
@@ -35,62 +34,60 @@ def fetch_population_data(): return json.loads(requests.get(
     'https://redutv-api.vg.no/corona/v1/sheets/norway-region-data/').text)
 
 # Get specific data from norway-info
-def get_total_contaminated(data):
-    return data["metadata"]["confirmed"]["total"]
+def get_total_contaminated(data): return data["metadata"]["confirmed"]["total"]
+def get_total_dead(data): return data["metadata"]["dead"]["total"]
 
-def get_total_dead(data):
-    return data["metadata"]["dead"]["total"]
-
-
-
+# Run soundcheck
 pygame.mixer.init()
 play_text("Startup")
 
-# Hospital data
+# Get data
 hospital_data = fetch_hospital_data()
-total_hospitalized = get_hospitalized_data(hospital_data)
-total_respiratory = get_respiratory_data(hospital_data)
-total_infectedEmployees = get_infectedEmployees_data(hospital_data)
-total_quarantineEmployees = get_quarantineEmployees_data(hospital_data)
-
-
-
-# Population data
 population_data = fetch_population_data()
-total_contaminated = get_total_contaminated(population_data)
-total_dead = get_total_dead(population_data)
 
-print('{:>15}  {:>15}  {:>15}  {:>15}'.format('Contaminated:', 'Confirmed dead:', 'Hospitalized:', 'Respiratory:'))
+statistics = {
+    "contaminated": {
+        "total": 0,
+        "getter": get_total_contaminated(population_data),
+        "announce": 'Contaminated: '
+    },
+    "dead": {
+        "total": 0,
+        "getter": get_total_dead(population_data),
+        "announce": 'Total dead: '
+    },
+    "hospitalized": {
+        "total": 0,
+        "getter": get_hospitalized_data(hospital_data),
+        "announce": 'Hospitalized: '
+    },
+    "respiratory": {
+        "total": 0,
+        "getter": get_respiratory_data(hospital_data),
+        "announce": 'People in respiration: '
+    },
+    "infected_employees": {
+        "total": 0,
+        "getter": get_infectedEmployees_data(hospital_data),
+        "announce": 'Infected employees: '
+    },
+    "quarantine_employees": {
+        "total": 0,
+        "getter": get_quarantineEmployees_data(hospital_data),
+        "announce": 'Quarantied employees: '
+    },
+    
+}
 
 while(True):
     # Run checks every 5 seconds
     time.sleep(5)
 
-    # Population data
+    # Fetch recent data
     population_data = fetch_population_data()
-    new_total_contaminated =  get_total_contaminated(population_data)
-    new_total_dead = get_total_dead(population_data)
-
-    # Hospital data
     hospital_data = fetch_hospital_data()
-    new_total_hospitalized = get_hospitalized_data(hospital_data)
-    new_total_respiratory = get_respiratory_data(hospital_data)
-    new_total_infectedEmployees = get_infectedEmployees_data(hospital_data)
-    new_total_quarantineEmployees = get_quarantineEmployees_data(hospital_data)
 
-    # Print current values
-    print('{:>15}  {:>15}  {:>15}  {:>15}'.format(new_total_contaminated, new_total_dead, new_total_hospitalized, new_total_respiratory))
-
-    # Check for update of the totals
-    if(total_contaminated < new_total_contaminated):
-        total_contaminated = new_total_contaminated
-        play_text("Contaminated: " + str(total_contaminated))
-    if(total_dead < new_total_dead):
-        total_dead = new_total_dead
-        play_text("Dead: " + str(total_dead))
-    if(total_hospitalized < new_total_hospitalized):
-        total_hospitalized = new_total_hospitalized
-        play_text("Hospitalized: " + str(total_hospitalized))
-    if(total_respiratory < new_total_respiratory):
-        new_total_respiratory = new_total_respiratory
-        play_text("Respiratory: " + str(total_respiratory))
+    for statistic in statistics:
+        if(statistics[statistic]['total'] < statistics[statistic]['getter']):
+            statistics[statistic]['total'] = statistics[statistic]['getter']
+            play_text(statistics[statistic]['announce']+str(statistics[statistic]['total']))
